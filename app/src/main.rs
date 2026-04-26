96a01aacf666245952f73c01712ce81c7fd86622
Robert Forster <robert.forster@beamvex.co.uk>
2026-04-26 10:41:47 +0100

    add chromiumoxide dependencies and implement basic browser connection to navigate to Amazon

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use chromiumoxide::browser::Browser;
+use futures::StreamExt;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let (browser, mut handler) = Browser::connect("http://127.0.0.1:9222").await?;
+
+    tokio::spawn(async move {
+        while let Some(_event) = handler.next().await {
+            let _ = _event;
+        }
+    });
+
+    let page = browser.new_page("https://amazon.com").await?;
+    page.wait_for_navigation().await?;
+
+    Ok(())
 }
